a03122d726dba1f8e03aeb23202d3631985b1d9e
Mouad Bouras <mouad.bouras@randstad.ca>
2019-10-08 21:56:36 -0400

    refactor search to use alphavantage component

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnDestroy, Output, EventEmitter } from '@angular/core';
 import { Observable, SubscriptionLike } from 'rxjs';
-import { TradierSecurity } from '../../models/search-response.model';
-import { TradierService } from '../../services/tradier.service';
-import { Security } from '../../../portfolios/model/security.model'
+import { AlphaVantageSecurity } from '../../models/search-response.model';
+//import { TradierService } from '../../services/tradier.service';
+import { Security } from '../../../portfolios/model/security.model';
 import { map, tap } from 'rxjs/operators';
+import { AlphavantageService } from '../../services/alphavantage.service';
+import { AlphaVantageQuote } from '../../models/quote-response.model';
 
 @Component({
   selector: 'app-search',
@@ -14,12 +16,16 @@ export class SearchComponent implements OnDestroy {
   @Output() securitySelected = new EventEmitter<Security>();
 
   public searchInput = '';
-  public securities$: Observable<TradierSecurity[]>;
+  public securities$: Observable<AlphaVantageSecurity[]>;
   public noResultsFound = false;
 
   private _subscription: SubscriptionLike;
+  public isLoading: boolean = false;
 
-  constructor( private _tradierService: TradierService ) { }
+  constructor(
+    //private _tradierService: TradierService,
+    private _alphavantageService: AlphavantageService,
+  ) { }
 
   ngOnDestroy() {
     if (this._subscription) {
@@ -30,30 +36,36 @@ export class SearchComponent implements OnDestroy {
   public search() {
     this.noResultsFound = false;
     if (this.searchInput.trim() !== '') {
-      this.securities$ = this._tradierService.searchSymbol(this.searchInput)
-      .pipe(
-        tap(securities => {
-          if (securities.length === 0) {
-            this.noResultsFound = true;
-          }
+      // this.securities$ = this._tradierService.searchSymbol(this.searchInput)
+      // .pipe(
+      //   tap(securities => {
+      //     if (securities.length === 0) {
+      //       this.noResultsFound = true;
+      //     }
+      //   })
+      // );
+      this.isLoading = true;
+      this.securities$ = this._alphavantageService.searchSymbol(this.searchInput).pipe(
+        tap( () => {
+          this.isLoading = false;
         })
       );
     }
 
   }
 
-  public selectSecurity(security: TradierSecurity) {
-    this.securities$ = new Observable<TradierSecurity[]>();
-    this._subscription = this._tradierService.getQuoteForSymbol(security.symbol).pipe(
+  public selectSecurity(security: AlphaVantageSecurity): void {
+    this.securities$ = new Observable<AlphaVantageSecurity[]>();
+    this._subscription = this._alphavantageService.getQuoteForSymbol(security.symbol).pipe(
       tap(console.log),
-      map((quote: TradierQuote) => {
+      map((quote: AlphaVantageQuote) => {
         return {
-          name: security.description,
+          name: security.name,
           symbol: security.symbol,
-          price: quote.last,
-          currency: quote.exch,
-          usPrice: quote.last,
-        } as Security;
+          price: quote.price,
+          currency: security.currency,
+          usPrice: quote.price,
+        } as unknown as Security;
       }),
       tap(console.log),
       tap((security: Security) => {
