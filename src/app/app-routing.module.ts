f21db21c2ad6ed4953f39c9ff0372e6f9a282bee
Mouad Bouras <mouad.bouras@randstad.ca>
2019-10-11 00:33:54 -0400

    cleanup up and prepare to deploy to firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { RegisterComponent } from './auth/components/register/register.component
 import { HomeComponent } from './core/home/home.component';
 import { LoginComponent } from './auth/components/login/login.component';
 import { AuthGuard } from './auth/guards/auth.guard';
-import { MainComponent } from './core/main/main.component';
+// import { MainComponent } from './core/main/main.component';
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 import { AdminComponent } from './admin/containers/admin/admin.component';
 import { AdminGuard } from './admin/guard/admin.guard';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, children: [
-    { path: '', component: MainComponent},
+  { path: '', component: HomeComponent,
+  children: [
+    { path: '',  loadChildren: './portfolios/portfolios.module#PortfoliosModule', canActivate: [AuthGuard]},
     { path: 'projects', loadChildren: './projects/projects.module#ProjectsModule', canActivate: [AuthGuard]},
     { path: 'customers', loadChildren: './customers/customers.module#CustomersModule', canActivate: [AuthGuard]},
     { path: 'profile', loadChildren: './profile/profile.module#ProfileModule', canActivate: [AuthGuard] },
